5f5ce3459b40b96d31571817d4855bbb41109e27
Marcin Mikolajczyk <mikol1980@gmail.com>
2020-02-21 15:01:01 +0100

    made changes

--- games/parkour_master_beta/2/js/moneta.js
+++ games/parkour_master_beta/2/js/moneta.js
@@ -1,5 +1,34 @@
 class Moneta {
-    construktor(ims, x, y, w, h) {
-        	
+    construktor(img, x, y, w, h) {
+            this.obraz = new Obraz(img, 960, 256, 16, 16);
+            this.animacja = {
+              obrot: {
+                klatka: [newObraz(img, 960, 256, 16, 16),
+                        new Obraz(img, 976, 256, 16, 16),
+                        new Obraz(img, 992, 256, 16, 16),
+                        new zadania.Obraz(img, 1008, 256, 16, 16)],
+                obecnaKlatka: 0
+              }
+            };
+            this.stan = {
+              obrot: {
+                animacja: (dane) => {
+                  if(dane.nrKlatki % 5 == 0) {
+                                  this.obraz = this.animacja.obrot.klatka[this.animacja.obrot.obecnaKlatka];
+                                  this.animacja.obrot.obecnaKlatka++;
+                              }
+                              
+                              if(this.animacja.obrot.obecnaKlatka > 3) {
+                                  this.animacja.obrot.obecnaKlatka = 0;
+                              }
+                }
+              }
+            };
+            this.obecnyStan = this.stan.obrot;
+            this.x = x;
+                  this.y = y;
+                  this.w = w;
+                  this.h = h;
+                  this.typ = "moneta";
     }
 }

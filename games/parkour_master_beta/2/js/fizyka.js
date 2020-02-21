04a67d51d2ed6aa4d52cb43947e42b761d4fe31d
Marcin Mikolajczyk <mikol1980@gmail.com>
2020-02-21 17:01:01 +0100

    made changes

---
+++ games/parkour_master_beta/2/js/fizyka.js
@@ -0,0 +1,24 @@
+class Fizyka {
+    aktualizacja(dane) {
+        this.grawitacja(dane.obiekty.mario);
+		//this.wykrywanieKolizji(dane);
+		this.smierc(dane);
+		
+		dane.obiekty.tabelaPotworow.forEach((p) => {
+			this.grawitacja(p);
+		//	this.wykrywanieKolizji2(dane, p);
+		});
+    }
+
+    grawitacja(obiekt) {
+        if(!obiekt.momentSmierci) obiekt.obecnyStan = obiekt.stan.skakanie;
+        obiekt.pedY+=1;
+        obiekt.y+=obiekt.pedY;
+    }
+
+    smierc(dane) {
+        if(dane.obiekty.mario.y > 624) 
+        dane.obiekty.mario.momentSmierci = true;
+        dane.kontroler.smierc.strataZycia(dane);
+    }
+}
--- games/parkour_master_beta/2/js/poruszanie.js
+++ games/parkour_master_beta/2/js/poruszanie.js
@@ -1,3 +1,15 @@
-class Poruszanie() {
+class Poruszanie {
+    aktualizacja(dane) {
+		this.Mario(dane);
+		this.Potwor(dane);
+	}
+	mario(dane) {
+    dane.obiekty.mario.obecnyStan.ruch(dane);
+    }
     
+    potwor(dane) {
+        dane.obiekty.tabelaPotworow.forEach(function(p) {
+            p.obecnyStan.ruch(dane);
+        });
+    }
 }

49191cbd68e4d04c2fe0a7b93e94b3bda7d1e34e
Marcin Mikolajczyk <mikol1980@gmail.com>
2020-02-25 20:01:02 +0100

    made changes

--- games/parkour_master_alpha/js/fizyka.js
+++ games/parkour_master_alpha/js/fizyka.js
@@ -22,5 +22,29 @@ class Fizyka {
         dane.kontroler.smierc.strataZycia(dane);
       }
     }
+
+    stronaKolizji(obiekt1, obiekt2) {
+      let maksymalnaOdlegloscX = (obiekt1.w + obiekt2.w)/2,
+        maksymalnaOdlegloscY = (obiekt1.h + obiekt2.h)/2;
+
+        let katLewyGorny = Math.atan2(maksymalnaOdlegloscY, maksymalnaOdlegloscX) * 180 / Math.PI,
+          KatPrawyGorny = 180 - katLewyGorny;
+
+          let odlegloscX = (obiekt2.x + obiekt2.w/2) - (obiekt1.x + obiekt1.w/2 - obiekt1.pedx),
+            odlegloscY = (obiekt2.y + obiekt2.h/2) - (obiekt1.y + obiekt1.h/2 - obiekt1.pedy);
+
+          let KatObiektow = Math.atan2(odlegloscY, odlegloscX) * 180 / Math.PI;
+
+          let stronaKolizji = [false, false, false, false];
+          if(KatObiektow > katLewyGorny && KatObiektow < KatPrawyGorny) {
+            stronaKolizji[0] = true;
+          }
+          if(KatObiektow > KatPrawyGorny || KatObiektow < -KatPrawyGorny) {
+            stronaKolizji[1] = true;
+          }
+          if(KatObiektow > -KatPrawyGorny && KatObiektow < -katLewyGorny) {
+            stronaKolizji[2] = true;
+          }
+    }
   }
   

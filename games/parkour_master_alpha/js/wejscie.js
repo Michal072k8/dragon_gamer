2de432b263b4d37d37bd797fc4d679df9d37fcd8
Marcin Mikolajczyk <mikol1980@gmail.com>
2020-03-19 19:01:02 +0100

    made changes

--- games/parkour_master_alpha/js/wejscie.js
+++ games/parkour_master_alpha/js/wejscie.js
@@ -13,10 +13,28 @@ class Wejscie {
   aktualizacja(dane) {
     let mario = dane.obiekty.mario;
 
-		if(this.nacisnieto(39)) {
+		if(this.nacisnieto(39) && !mario.momentSmierci) {
 			mario.kierunek = "prawo";
+			mario.pedX = 8;
+
+			if(mario.pedY == 0) {
+				mario.obecnyStan = mario.poruszanie;
+			}else {
+				mario.obecnyStan = mario.poruszanie;
+			}
+			
+		}
+		if(this.nacisnieto(37) && !mario.momentSmierci) {
+			mario.kierunek = "lewo";
+			mario.pedX = -8;
 
 			
+			if(mario.pedY == 0) {
+				mario.obecnyStan = mario.poruszanie;
+			}else {
+				mario.obecnyStan = mario.poruszanie;
+			}
+
 		}
 		if(this.nacisnieto(32)) {
 			mario.obecnyStan = mario.stan.skakanie;
